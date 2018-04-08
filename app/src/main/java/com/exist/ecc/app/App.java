e85a615dc3353393bf0c2111a65fe9224027ab9e
Mikhail B. Nunez <mikhailbnunez@gmail.com>
2018-04-09 04:42:18 +0800

    Initial Project Structure

---
+++ app/src/main/java/com/exist/ecc/app/App.java
@@ -0,0 +1,10 @@
+package com.exist.ecc.app;
+
+import com.exist.ecc.core.service.Test;
+
+public class App {
+      public static void main( String[] args ) {
+            System.out.println( "Hello World!" );
+            new Test().printTest();
+      }
+}
---
+++ core/service/src/main/java/com/exist/ecc/core/service/Test.java
@@ -0,0 +1,7 @@
+package com.exist.ecc.core.service;
+
+public class Test {
+      public void printTest() {
+            System.out.println( "Inside Teest Class" );
+      }
+}
